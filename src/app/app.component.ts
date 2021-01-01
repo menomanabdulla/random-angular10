c7dd4289634b6c6dea79f26a160933c81e088144
noman abdulla <noman.fcibappy@gmail.com>
2021-01-01 07:26:35 +0600

    ng-conditional statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,30 @@ export class AppComponent {
   enableBox(){
     this.disableedBox = false
   }
+  //Ng-Conditional statement
+  /*
+    Condition in Angular
+      1. Make If Condition
+      2. Make If else Condition
+      3. Make Condition with string
+      4. Make elseif Condition
+      5. Make Conditon with property Binding
+  */
+
+  //1. Make If Condition
+  //2. Make If else Condition
+  show = false;
+  toogleBLock(){
+    this.show = !this.show;
+  }
+  //3. Make Condition with string
+  //4. Make elseif Condition
+  showText = 'yes'
+  toogleBLock2(){
+    this.showText = this.showText == 'yes' ? 'no' : 'yes';
+  }
+
+  //5. Make Conditon with property Binding
+  color = 'blue'
 }
+
